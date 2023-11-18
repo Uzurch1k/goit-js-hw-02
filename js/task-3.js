351b37a8cde6bcc78c86f5c7f4cecfbfa69f4a1d
Uzurch1k <121051200+Uzurch1k@users.noreply.github.com>
2023-11-18 21:33:40 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+//====================================================================
+function getElementWidth(content, padding, border) {
+  let contentSize = Number.parseFloat(content);
+
+  let paddingSize = Number.parseFloat(padding);
+  paddingSize *= 2;
+
+  let borderSize = Number.parseFloat(border);
+  borderSize *= 2;
+
+  const borderBox = contentSize + paddingSize + borderSize;
+
+  return borderBox;
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
