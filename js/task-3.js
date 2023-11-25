a28b982391893eb030608896104eb23122bcb48c
Uzurch1k <galcenkovlad7@gmail.com>
2023-11-25 15:41:07 +0200

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,9 @@
 //====================================================================
 function checkForSpam(message) {
-  const messageLower = message.toLowerCase();
-  return messageLower.includes('spam') || messageLower.includes('sale');
+  const normalizedMessage = message.toLowerCase();
+  return (
+    normalizedMessage.includes('spam') || normalizedMessage.includes('sale')
+  );
 }
 
 console.log(checkForSpam('Latest technology news')); // false
